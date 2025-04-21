3fdc87b1e553a603dc87d785f2e8be8d73a3b099
Yuvraj <ykyuvrajkashyap@gmail.com>
2025-04-21 18:31:36 -0500

    final update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config import APARTMENT, MAKE, MODEL, PLATE
+from config_template import APARTMENT, MAKE, MODEL, PLATE
 import time
 
 
