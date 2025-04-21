a739efa9314c3121efe8e7ae46ffda9240689415
Yuvraj <ykyuvrajkashyap@gmail.com>
2025-04-21 18:36:36 -0500

    again.. test

---
+++ config_template.py
@@ -0,0 +1,6 @@
+# config_template.py (included in GitHub)
+APARTMENT = "YOUR_APT_HERE"
+MAKE = "YOUR_CAR_MAKE"
+MODEL = "YOUR_CAR_MODEL"
+PLATE = "YOUR_LICENSE_PLATE"
+# fun fact: i put my actual info here at first like an idiot :)
